e44f0cc32b664fcb3c7af4d35be7235cfcc04994
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2015-05-21 14:55:55 +0200

    drop ntpdate, use the ntp daemon instead

--- src/vagrant/bootstrap.sh
+++ src/vagrant/bootstrap.sh
@@ -24,8 +24,7 @@ echo "192.168.50.4 domsgui-testbed" >> /etc/hosts
 
 #The host machine might have a screwed clock, so set it correctly in the vagrant
 echo "server ntp.statsbiblioteket.dk" > /etc/ntp.conf #we do not want it synchronizing with odd servers
-sudo service ntp stop # Restart the ntp service so that only the ntp.statsbiblioteket.dk is used
-sudo ntpdate ntp.statsbiblioteket.dk # And make sure the date is now set from ntp server
+sudo service ntp restart # Restart the ntp service so that only the ntp.statsbiblioteket.dk is used
 
 
 #sudo su - vagrant -c "bash /vagrant/doms.sh"
